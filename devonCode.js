5fcca2c9b5d3b75a1e0ee1eeffada994e1a8581e
Devon Truman <dtruman@student.neumont.edu>
2014-08-27 14:27:47 +0000

    Player
    git-svn-id: https://svn.neumont.edu:8443/svn/gat280finalgame@15 d9cdbc9e-7787-8544-b69c-d47181f1b44e

--- devonCode.js
+++ devonCode.js
@@ -1,38 +1,42 @@
 //Devon's Code
 
-var rectangleMan;
 var movementSpeed=5;
 var bulRep;
 
+function Player()
+{
+	CharacterObject.call( this );
+	this.radius = 32;
+}
+
+Player.prototype = Object.create(CharacterObject.prototype);
+Player.prototype.constructor = Player;
+Player.prototype.innerUpdate = function( dt )
+{
+	if(isKeyDown("W"))
+       this.y-=movementSpeed;
+    if(isKeyDown("S"))
+        this.y+=movementSpeed;
+    if(isKeyDown("A"))
+        this.x-=movementSpeed;
+    if(isKeyDown("D"))
+        this.x+=movementSpeed;
+}
+
 function initDevon()
 {
-    rectangleMan=new createjs.Shape();
-    rectangleMan.graphics.beginFill("#447").drawRect(0,0,20,20);
-    rectangleMan.x=300;
-    rectangleMan.y=100;
-    gameStage.addChild(rectangleMan);
     bulRep=new Array();
 }
 
 function runDevon( dt )
-{
-    rectangleMan.visible=true;
-    if(isKeyDown("W"))
-       rectangleMan.y-=movementSpeed;
-    if(isKeyDown("S"))
-        rectangleMan.y+=movementSpeed;
-    if(isKeyDown("A"))
-        rectangleMan.x-=movementSpeed;
-    if(isKeyDown("D"))
-        rectangleMan.x+=movementSpeed;
-    
+{   
     if(isMousePressed())
     {
         var bullet=new createjs.Shape();
         bullet.graphics.beginFill("#447").drawCircle(0,0,10);
-        bullet.x=rectangleMan.x;
-        bullet.y=rectangleMan.y;
-        var vec=new vector2D(getMouseX()-rectangleMan.x, getMouseY()-rectangleMan.y);
+        bullet.x=Player.x;
+        bullet.y=Player.y;
+        var vec=new vector2D(getMouseX()-Player.x, getMouseY()-Player.y);
         bulRep.push(new Bullet(vec, bullet));
         gameStage.addChild(bulRep[bulRep.length-1].bullet);
     }
