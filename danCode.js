b941c658aa010249834f9193cb65c2d3742fde99
Daniel Nipper <dnipper2011@gmail.com>
2014-09-01 22:08:32 +0000

    AI shoot slower
    git-svn-id: https://svn.neumont.edu:8443/svn/gat280finalgame@76 d9cdbc9e-7787-8544-b69c-d47181f1b44e

--- danCode.js
+++ danCode.js
@@ -10,7 +10,7 @@ function EnemyCharacter()
     this.alignment=1;
     this.health=100;
     this.shoots=false;
-    this.attackrate=10;
+    this.attackrate=40;
     this.isActive=false;
     this.wait=0;
     this.targetX=0;
